faceec19fd32b12840b337653ac38d551e980077
= <=>
2018-12-22 15:02:36 -0200

    Exception handling for delete (referential integrity)

---
+++ SalesWebMvc/Services/Exceptions/IntegrityException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Services.Exceptions
+{
+    public class IntegrityException : ApplicationException
+    {
+        // ssbcvp - erros de integridade referencial de banco serão tratados aqui
+        public IntegrityException(string message) : base(message)
+        {
+
+        }
+    }
+}
--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -41,9 +41,17 @@ namespace SalesWebMvc.Services
 
         public async Task RemoveAsync(int id)
         {
-            var obj = await _context.Seller.FindAsync(id);
+            try
+            {
+                var obj = await _context.Seller.FindAsync(id);
             _context.Seller.Remove(obj);
-            await _context.SaveChangesAsync();
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException e)
+            {
+                // ssbcvp - estamos lançando a nossa excessão personalizada
+                throw new IntegrityException("Can't delete seller because he/she has sales");
+            }
         }
 
         public async Task UpdateAsync(Seller obj)
